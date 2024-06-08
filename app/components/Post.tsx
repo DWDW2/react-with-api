e038ed447d9686caf687c7ed87464bb035c83afb
DWDW2 <daid8002@gmail.com>
2024-06-08 11:34:06 +0500

    init_set-up

--- app/components/Post.tsx
+++ app/components/Post.tsx
@@ -4,11 +4,13 @@ type Props = {
     title: string,
     body: string,
     tags: string[],
+    link: string
 }
 
-export default function Post({title, body, tags}: Props) {
+export default function Post({title, body, tags, link}: Props) {
   return (
-    <div className='flex items-center justify-center mb-5 mt-5'>  
+    <div className='flex items-center justify-center mb-5 mt-5'>
+       
             <div className="rounded-xl border p-5 shadow-md w-9/12 bg-white">
             <div className="flex w-full items-center justify-between border-b pb-3">
             <div className="flex items-center space-x-8">
@@ -24,8 +26,8 @@ export default function Post({title, body, tags}: Props) {
             </div>
 
             <div className="mt-4 mb-6">
-            <Link href='/'> 
-                <div className="mb-3 text-xl font-bold">{title}</div>
+            <Link href={link}>
+                    <div className="mb-3 text-xl font-bold">{title}</div>
             </Link>
             <div className="text-sm text-neutral-600">{body}</div>
             </div>
@@ -48,7 +50,7 @@ export default function Post({title, body, tags}: Props) {
                 </div>
             </div>
             </div>
-        </div>
+    </div>
     </div>
   )
 }
--- app/posts/[id]/page.tsx
+++ app/posts/[id]/page.tsx
@@ -1,12 +1,53 @@
+'use client'
+import Post from '@/app/components/Post'
+import axios from 'axios'
 import React from 'react'
+
+type DataPost = {
+  posts : Array<Data>,
+  total: number,
+  skip: number,
+  limit: number
+}
+
+type Data = {
+  id: number,
+  title: string,
+  body: string,
+  tags: string[],
+  reactions: object,
+  views: number,
+  userId: number
+}
+
+type DataArr = Array<Data>
+
 
 type Props = {
     params: {id: string}
 }
 
+async function fetchData(): Promise<DataPost> {
+  const response = await axios.get('https://dummyjson.com/posts');
+  const data: DataPost = response.data;
+  console.log(data);
+  return data;
+}
+
 export default function PostID({params}: Props) {
-    console.log(params)
+  const [data, setData] = React.useState<DataArr | null>(null);
+
+    React.useEffect(() => {
+      fetchData().then((data) => setData(data.posts));
+    }, []);
+  
+    if (!data) {
+      return <div>Loading...</div>;
+    };
   return (
-    <div>{params.id}</div>
+    data && data.map((post: Data) => {
+      if (post.id === Number(params.id))
+      return (<Post key={post.id} title={post.title} body={post.body} tags={post.tags} link={`posts/${post.id}`} />)
+    }),
   )
 }
